f93cf34e57de722c42cf3229ddf01ef42ec79d07
Stefano Frontini <stefanofrontini75@gmail.com>
2024-01-11 14:56:43 +0100

    returns Either

--- src/formatter/renameField.ts
+++ src/formatter/renameField.ts
@@ -1,28 +1,15 @@
 import * as E from "fp-ts/Either";
-import * as O from "fp-ts/Option";
 import { modifyAt } from "fp-ts/Record";
-import { identity, pipe } from "fp-ts/function";
+import { pipe } from "fp-ts/function";
 
-const jsonParse: <T>(
-  text: string
-) => E.Either<Error, Record<string, T>> = E.tryCatchK(JSON.parse, E.toError);
-
-export const renameField = <T>(
-  input: string,
-  field: keyof Record<string, T>,
-  newField: T
-): string =>
+export const renameField = <T extends Record<string, unknown>>(
+  input: T,
+  field: keyof T,
+  newField: unknown
+): E.Either<Error, T> =>
   pipe(
-    jsonParse<T>(input),
-    E.bimap(
-      err => `Cannot parse JSON: ${err.message}`,
-      parsedJson =>
-        pipe(
-          parsedJson,
-          modifyAt(field, () => newField),
-          O.getOrElseW(() => `Cannot find field: ${field}`),
-          JSON.stringify
-        )
-    ),
-    E.getOrElse(identity)
+    input,
+    modifyAt(String(field), () => newField),
+    E.fromOption(() => new Error(`Field ${String(field)} not found`)),
+    E.map(r => r as T)
   );
