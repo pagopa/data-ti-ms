b39dc5b539720110338117b893b0d9fc15f36b04
Stefano Frontini <stefanofrontini75@gmail.com>
2024-01-11 15:38:13 +0100

    fix test

--- src/formatter/__test__/renameField.test.ts
+++ src/formatter/__test__/renameField.test.ts
@@ -1,25 +1,14 @@
+import * as E from "fp-ts/Either";
 import { renameField } from "../renameField";
 
-const jsonString = '{"bar": 1, "foo": "foo"}';
-const invalidJsonString = '{"bar" 1, "foo": "foo"}';
+const inputData = {
+  bar: "bar",
+  foo: "foo"
+};
 
 describe("renameField", () => {
   it("should rename a field", () => {
-    const outputString = renameField<string | number>(jsonString, "bar", 2);
-    expect(outputString).toEqual('{"bar":2,"foo":"foo"}');
-  });
-  it("should output a parsing error", () => {
-    const outputString = renameField<string | number>(
-      invalidJsonString,
-      "bar",
-      2
-    );
-    expect(outputString).toEqual(
-      "Cannot parse JSON: Expected ':' after property name in JSON at position 7"
-    );
-  });
-  it("should output a missing field error", () => {
-    const outputString = renameField<string | number>(jsonString, "baz", 2);
-    expect(outputString).toEqual('"Cannot find field: baz"');
+    const result = renameField(inputData, "bar", 2);
+    expect(result).toEqual(E.right({ bar: 2, foo: "foo" }));
   });
 });
