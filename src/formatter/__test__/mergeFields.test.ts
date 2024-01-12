98ce84f78daf7257b93b72e6089707785daefe94
Stefano Frontini <stefanofrontini75@gmail.com>
2024-01-12 17:46:18 +0100

    returns T instead of Either and adds more tests

--- src/formatter/__test__/mergeFields.test.ts
+++ src/formatter/__test__/mergeFields.test.ts
@@ -1,4 +1,3 @@
-import * as E from "fp-ts/Either";
 import { mergeFields } from "../mergeFields";
 
 const inputData = {
@@ -10,8 +9,34 @@ const inputData = {
 describe("mergeFields", () => {
   it("should merge fields into a new field", () => {
     const res = mergeFields(inputData, ["foo", "bar"], "mergedField", "-");
-    expect(res).toEqual(
-      E.right({ bar: 1, baz: "hello", foo: "Foo", mergedField: "Foo-1" })
+    expect(res).toEqual({
+      bar: 1,
+      baz: "hello",
+      foo: "Foo",
+      mergedField: "Foo-1"
+    });
+  });
+  it("should output an empty string if no fields are provided", () => {
+    const res = mergeFields(inputData, [], "mergedField", "-");
+    expect(res).toEqual({
+      bar: 1,
+      baz: "hello",
+      foo: "Foo",
+      mergedField: ""
+    });
+  });
+  it("should output an empty string if a missing field is provided", () => {
+    const res = mergeFields(
+      inputData,
+      ["bas" as keyof typeof inputData],
+      "mergedField",
+      "-"
     );
+    expect(res).toEqual({
+      bar: 1,
+      baz: "hello",
+      foo: "Foo",
+      mergedField: ""
+    });
   });
 });
