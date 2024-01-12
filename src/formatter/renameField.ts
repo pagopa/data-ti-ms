3cb35e0096b9aceafac1cf898c21135c8e6129b8
AleDore <alessio85.dore@gmail.com>
2024-01-12 17:38:19 +0100

    better typing

--- src/formatter/renameField.ts
+++ src/formatter/renameField.ts
@@ -5,18 +5,18 @@ import { pipe } from "fp-ts/function";
 
 type NotInKeys<T, K extends string> = K extends keyof T ? never : K;
 
-export const renameField = <T, K extends string>(
+export const renameField = <T, R extends keyof T, K extends string>(
   input: T,
-  field: keyof T,
+  field: R,
   newField: NotInKeys<T, K>
 ): T =>
   pipe({ ...input, [field]: undefined, [newField]: input[field] }, res =>
     withoutUndefinedValues(res)
   );
 
-export const renameFields = <T, K extends string>(
+export const renameFields = <T, R extends keyof T, K extends string>(
   input: T,
-  renameChanges: ReadonlyArray<ITuple2<keyof T, NotInKeys<T, K>>>
+  renameChanges: ReadonlyArray<ITuple2<R, NotInKeys<T, K>>>
 ): T =>
   renameChanges.reduce(
     (acc, currChange) => renameField(acc, currChange.e1, currChange.e2),
