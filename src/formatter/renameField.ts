c0369c2553d07d713835eab228c7967c841187e7
AleDore <alessio85.dore@gmail.com>
2024-01-12 16:46:49 +0100

    fix newField type

--- src/formatter/renameField.ts
+++ src/formatter/renameField.ts
@@ -3,14 +3,20 @@ import { withoutUndefinedValues } from "@pagopa/ts-commons/lib/types";
 
 import { pipe } from "fp-ts/function";
 
-export const renameField = <T>(input: T, field: keyof T, newField: string): T =>
+type NotInKeys<T, K extends string> = K extends keyof T ? never : K;
+
+export const renameField = <T, K extends string>(
+  input: T,
+  field: keyof T,
+  newField: NotInKeys<T, K>
+): T =>
   pipe({ ...input, [field]: undefined, [newField]: input[field] }, res =>
     withoutUndefinedValues(res)
   );
 
-export const renameFields = <T>(
+export const renameFields = <T, K extends string>(
   input: T,
-  renameChanges: ReadonlyArray<ITuple2<keyof T, string>>
+  renameChanges: ReadonlyArray<ITuple2<keyof T, NotInKeys<T, K>>>
 ): T =>
   renameChanges.reduce(
     (acc, currChange) => renameField(acc, currChange.e1, currChange.e2),
