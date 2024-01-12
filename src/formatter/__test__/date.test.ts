737fd7b8170f650d9573675f3e8a5b2aa822e608
Stefano Frontini <stefanofrontini75@gmail.com>
2024-01-12 17:20:56 +0100

    functions returns Either and adds more tests

--- src/formatter/__test__/date.test.ts
+++ src/formatter/__test__/date.test.ts
@@ -1,7 +1,7 @@
+import * as E from "fp-ts/Either";
 import {
   convertFormat,
   dateStringFromTimestampFormat,
-  dateStringToDateObject,
   dateStringToIsoFormat,
   dateStringToTimeStampFormat,
   dateStringToUtcFormat,
@@ -11,84 +11,91 @@ import {
 const isoStr = "2023-01-01T12:34:56.789Z";
 const invalidIsoStr = "202301-01T12:34:56.789Z";
 const dateStr = "2023-12-19";
+const invalidDateString = "202313-19";
 const isoTimeZone = "2018-10-13T00:00:00.000+00:00";
 const timestamp = 1702944000000;
+const invalidOutputFormat = "YYY-MM-dd";
+const outputFormat1 = "YYYY-MM-dd";
+const invalidDateStringError = new Error("Invalid dateString");
 
 describe("dateStringToUtcFormat", () => {
   it("should return a UTC string from a date string like: 2023-12-19", () => {
     const utc = dateStringToUtcFormat(dateStr);
-    expect(utc).toEqual("Tue, 19 Dec 2023 00:00:00 GMT");
+    expect(utc).toEqual(E.right("Tue, 19 Dec 2023 00:00:00 GMT"));
   });
   it("should return a UTC string from a ISO string", () => {
     const utc = dateStringToUtcFormat(isoStr);
-    expect(utc).toEqual("Sun, 01 Jan 2023 12:34:56 GMT");
+    expect(utc).toEqual(E.right("Sun, 01 Jan 2023 12:34:56 GMT"));
   });
   it("should return a UTC string from a IsoTimeZone string", () => {
     const utc = dateStringToUtcFormat(isoTimeZone);
-    expect(utc).toEqual("Sat, 13 Oct 2018 00:00:00 GMT");
+    expect(utc).toEqual(E.right("Sat, 13 Oct 2018 00:00:00 GMT"));
   });
-  it("should return an error from an invalid ISO string", () => {
+  it("should return an error from a not valid ISO string", () => {
     const error = dateStringToUtcFormat(invalidIsoStr);
-    expect(error).toEqual(
-      'Invalid value "202301-01T12:34:56.789Z" supplied to : DateFromString'
-    );
-  });
-});
-
-describe("dateStringToDateObject", () => {
-  it("should return a date object from a date string like: 2023-12-19", () => {
-    const date = dateStringToDateObject(dateStr);
-    expect(date).toEqual(new Date("2023-12-19"));
-  });
-  it("should return a date object from a ISO string", () => {
-    const date = dateStringToDateObject(isoStr);
-    expect(date).toEqual(new Date("2023-01-01T12:34:56.789Z"));
-  });
-  it("should return a date object from a IsoTimeZone string", () => {
-    const date = dateStringToDateObject(isoTimeZone);
-    expect(date).toEqual(new Date("2018-10-13T00:00:00.000+00:00"));
+    expect(error).toEqual(E.left(invalidDateStringError));
   });
 });
 
 describe("isoToUtcFormat", () => {
   it("should return a UTC string from a ISO string", () => {
     const utc = isoToUtcFormat(isoStr);
-    expect(utc).toEqual("Sun, 01 Jan 2023 12:34:56 GMT");
+    expect(utc).toEqual(E.right("Sun, 01 Jan 2023 12:34:56 GMT"));
+  });
+  it("should return an error from an invalid ISO string", () => {
+    const error = isoToUtcFormat(invalidIsoStr);
+    expect(error).toEqual(E.left(new Error("Invalid isoString")));
   });
 });
 
 describe("dateStringToIsoFormat", () => {
   it("should return a ISO string from a date string like: 2023-12-19", () => {
     const iso = dateStringToIsoFormat(dateStr);
-    expect(iso).toEqual("2023-12-19T00:00:00.000Z");
+    expect(iso).toEqual(E.right("2023-12-19T00:00:00.000Z"));
+  });
+  it("should return an error from an invalid date string", () => {
+    const error = dateStringToIsoFormat(invalidDateString);
+    expect(error).toEqual(E.left(invalidDateStringError));
   });
 });
 
 describe("dateStringToTimeStampFormat", () => {
   it("should return a timestamp from a date string like: 2023-12-19", () => {
     const ts = dateStringToTimeStampFormat(dateStr);
-    expect(ts).toEqual(ts);
+    expect(ts).toEqual(E.right(timestamp));
+  });
+  it("should return an error from an invalid date string", () => {
+    const error = dateStringToTimeStampFormat(invalidDateString);
+    expect(error).toEqual(E.left(invalidDateStringError));
   });
 });
 
 describe("dateStringFromTimestampFormat", () => {
   it("should return a date string from a timestamp", () => {
     const dateString = dateStringFromTimestampFormat(timestamp);
-    expect(dateString).toEqual("Tue Dec 19 2023");
+    expect(dateString).toEqual(E.right("Tue Dec 19 2023"));
   });
 });
 
 describe("convertFormat", () => {
   it("should return a date string YYYY-MM-dd from a ISO string", () => {
-    const dateString = convertFormat(isoStr, "YYYY-MM-dd");
-    expect(dateString).toEqual("2023-01-01");
+    const dateString = convertFormat(isoStr, outputFormat1);
+    expect(dateString).toEqual(E.right("2023-01-01"));
   });
   it("should return a date string YYYY-MM-dd hh:mm from a ISO string", () => {
     const dateString = convertFormat(isoStr, "YYYY-MM-dd hh:mm");
-    expect(dateString).toEqual("2023-01-01 12:34");
+    expect(dateString).toEqual(E.right("2023-01-01 12:34"));
   });
   it("should return a date string YYYY-MM-dd hh:mm:ss from a ISO string", () => {
     const dateString = convertFormat(isoStr, "YYYY-MM-dd hh:mm:ss");
-    expect(dateString).toEqual("2023-01-01 12:34:56");
+    expect(dateString).toEqual(E.right("2023-01-01 12:34:56"));
+  });
+  it("should return an error from an invalid ISO string", () => {
+    const error = convertFormat(invalidIsoStr, outputFormat1);
+    expect(error).toEqual(E.left(new Error("Invalid isoString")));
+  });
+  it("should return an error from an invalid outputFormat", () => {
+    const error = convertFormat(isoStr, invalidOutputFormat as "YYYY-MM-dd");
+    expect(error).toEqual(E.left(new Error("Invalid output format")));
   });
 });
--- src/formatter/date.ts
+++ src/formatter/date.ts
@@ -4,51 +4,58 @@ import {
   UtcOnlyIsoDateFromString
 } from "@pagopa/ts-commons/lib/dates";
 import * as E from "fp-ts/Either";
-import { identity, pipe } from "fp-ts/function";
+import { pipe } from "fp-ts/function";
 import * as t from "io-ts";
-import { failure } from "io-ts/PathReporter";
 
-const onLeft = (errors: t.Errors): string =>
-  pipe(failure(errors), e => e.join("\n"));
+const invalidDateStringError = (): Error => new Error("Invalid dateString");
 
-export const dateStringToUtcFormat = (dateString: string): string =>
+export const dateStringToUtcFormat = (
+  dateString: string
+): E.Either<Error, string> =>
   pipe(
     dateString,
     DateFromString.decode,
-    E.match(onLeft, date => date.toUTCString())
+    E.bimap(invalidDateStringError, date => date.toUTCString())
   );
 
-export const dateStringToDateObject = (dateString: string): string | Date =>
-  pipe(dateString, DateFromString.decode, E.getOrElseW(onLeft));
-
-export const isoToUtcFormat = (isoString: string): string =>
+export const isoToUtcFormat = (isoString: string): E.Either<Error, string> =>
   pipe(
     isoString,
     UtcOnlyIsoDateFromString.decode,
-    E.match(onLeft, date => date.toUTCString())
+    E.bimap(
+      () => new Error("Invalid isoString"),
+      date => date.toUTCString()
+    )
   );
 
-export const dateStringToIsoFormat = (dateString: string): string =>
+export const dateStringToIsoFormat = (
+  dateString: string
+): E.Either<Error, string> =>
   pipe(
     dateString,
     DateFromString.decode,
-    E.match(onLeft, date => date.toISOString())
+    E.bimap(invalidDateStringError, date => date.toISOString())
   );
 
 export const dateStringToTimeStampFormat = (
   dateString: string
-): string | number =>
+): E.Either<Error, number> =>
   pipe(
     dateString,
     DateFromString.decode,
-    E.matchW(onLeft, date => date.getTime())
+    E.bimap(invalidDateStringError, date => date.getTime())
   );
 
-export const dateStringFromTimestampFormat = (ts: number): string =>
+export const dateStringFromTimestampFormat = (
+  ts: number
+): E.Either<Error, string> =>
   pipe(
     ts,
     DateFromTimestamp.decode,
-    E.matchW(onLeft, date => date.toDateString())
+    E.bimap(
+      () => new Error("Invalid timestamp"),
+      date => date.toDateString()
+    )
   );
 
 const OutputFormat = t.union([
@@ -62,34 +69,36 @@ type OutputFormat = t.TypeOf<typeof OutputFormat>;
 export const convertFormat = (
   isoString: string,
   output: OutputFormat
-): string =>
+): E.Either<Error, string> =>
   pipe(
     output,
     OutputFormat.decode,
-    E.mapLeft(onLeft),
+    E.mapLeft(() => new Error("Invalid output format")),
     E.flatMap(outputFormat =>
       pipe(
         isoString,
         DateFromString.decode,
-        E.bimap(onLeft, date => {
-          const year = date.getUTCFullYear();
-          const month = String(date.getUTCMonth() + 1).padStart(2, "0");
-          const day = String(date.getUTCDate()).padStart(2, "0");
-          const hours = String(date.getUTCHours()).padStart(2, "0");
-          const minutes = String(date.getUTCMinutes()).padStart(2, "0");
-          const seconds = String(date.getUTCSeconds()).padStart(2, "0");
+        E.bimap(
+          () => new Error("Invalid isoString"),
+          date => {
+            const year = date.getUTCFullYear();
+            const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+            const day = String(date.getUTCDate()).padStart(2, "0");
+            const hours = String(date.getUTCHours()).padStart(2, "0");
+            const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+            const seconds = String(date.getUTCSeconds()).padStart(2, "0");
 
-          if (outputFormat === "YYYY-MM-dd") {
-            return `${year}-${month}-${day}`;
+            if (outputFormat === "YYYY-MM-dd") {
+              return `${year}-${month}-${day}`;
+            }
+            if (outputFormat === "YYYY-MM-dd hh:mm") {
+              return `${year}-${month}-${day} ${hours}:${minutes}`;
+            }
+            if (outputFormat === "YYYY-MM-dd hh:mm:ss") {
+              return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+            }
           }
-          if (outputFormat === "YYYY-MM-dd hh:mm") {
-            return `${year}-${month}-${day} ${hours}:${minutes}`;
-          }
-          if (outputFormat === "YYYY-MM-dd hh:mm:ss") {
-            return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-          }
-        })
+        )
       )
-    ),
-    E.getOrElse(identity)
+    )
   );
