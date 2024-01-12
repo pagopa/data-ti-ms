f6f2e4c0a80a808c5cff5f96db77b20a4155af0f
AleDore <alessio85.dore@gmail.com>
2024-01-12 10:31:26 +0100

    Add multiple renameFields + tests

--- src/formatter/renameField.ts
+++ src/formatter/renameField.ts
@@ -1,3 +1,4 @@
+import { ITuple2 } from "@pagopa/ts-commons/lib/tuples";
 import { withoutUndefinedValues } from "@pagopa/ts-commons/lib/types";
 
 import { pipe } from "fp-ts/function";
@@ -5,4 +6,13 @@ import { pipe } from "fp-ts/function";
 export const renameField = <T>(input: T, field: keyof T, newField: string): T =>
   pipe({ ...input, [field]: undefined, [newField]: input[field] }, res =>
     withoutUndefinedValues(res)
+  );
+
+export const renameFields = <T>(
+  input: T,
+  renameChanges: ReadonlyArray<ITuple2<keyof T, string>>
+): T =>
+  renameChanges.reduce(
+    (acc, currChange) => renameField(acc, currChange.e1, currChange.e2),
+    input
   );
