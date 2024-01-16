2f3b815498a94d76ebbe2376a2c6e317e2ca5466
AleDore <alessio85.dore@gmail.com>
2024-01-16 10:52:31 +0100

    Add tests + refactor

--- src/enrichment/table.ts
+++ src/enrichment/table.ts
@@ -1,8 +1,19 @@
 import { pipe } from "fp-ts/lib/function";
 import * as TE from "fp-ts/TaskEither";
+import * as E from "fp-ts/Either";
 import * as O from "fp-ts/Option";
 import * as DT from "@azure/data-tables";
+import * as t from "io-ts";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { errorsToReadableMessages } from "@pagopa/ts-commons/lib/reporters";
 import { getTableDocument } from "../utils/tableStorage";
+
+export const InputKeyFields = t.type({
+  partitionKey: NonEmptyString,
+  rowKey: NonEmptyString
+});
+
+export type InputKeyFields = t.TypeOf<typeof InputKeyFields>;
 
 export const tableEnrich = <T>(
   input: T,
@@ -11,10 +22,18 @@ export const tableEnrich = <T>(
   rowKeyField: keyof T
 ): TE.TaskEither<Error, T> =>
   pipe(
-    getTableDocument(
-      tableClient,
-      String(input[partitionKeyField]),
-      String(input[rowKeyField])
+    { partitionKey: input[partitionKeyField], rowKey: input[rowKeyField] },
+    InputKeyFields.decode,
+    E.mapLeft(errs =>
+      Error(
+        `Given key fields from input stream are not valid|Error=${errorsToReadableMessages(
+          errs
+        )}`
+      )
+    ),
+    TE.fromEither,
+    TE.chain(inputKeys =>
+      getTableDocument(tableClient, inputKeys.partitionKey, inputKeys.rowKey)
     ),
     TE.map(
       O.fold(
