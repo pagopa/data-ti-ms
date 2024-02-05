0ebe33cf43ec1f0900d59240ab1b2a784db5e2dd
Davide Marro <davide.marro@pagopa.it>
2024-02-05 16:41:36 +0100

    First commit

---
+++ src/deduplication/__tests__/elasticsearch.test.ts
@@ -0,0 +1,264 @@
+import * as EL from "@elastic/elasticsearch";
+import * as TE from "fp-ts/TaskEither";
+import { pipe } from "fp-ts/lib/function";
+import {
+  IOutputDocument,
+  createIndex,
+  createIndexIfNotExists,
+  getDocument,
+  getElasticClient,
+  indexDocument,
+  updateIndexDocument
+} from "../elasticsearch";
+
+const mockCreate = jest.fn();
+const mockExists = jest.fn();
+const mockGet = jest.fn();
+const mockIndex = jest.fn();
+const mockUpdate = jest.fn();
+
+jest.mock("@elastic/elasticsearch", () => ({
+  Client: jest.fn()
+}));
+
+const mockElasticClient: EL.Client = ({
+  indices: {
+    create: mockCreate,
+    exists: mockExists
+  },
+  get: mockGet,
+  index: mockIndex,
+  update: mockUpdate
+} as unknown) as EL.Client;
+
+const elasticNode = "http://localhost:9200";
+const indexName = "test_index";
+const document: IOutputDocument = {
+  id: "123",
+  key1: "value1",
+  key2: "value2"
+};
+
+describe("getElasticClient", () => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("getElasticClient should return an instance of Elasticsearch client", async () => {
+    (EL.Client as jest.Mock).mockImplementationOnce(() => mockElasticClient);
+    await pipe(
+      getElasticClient(elasticNode),
+      TE.bimap(
+        () => {
+          new Error(`it should not fail while finding an existing document`);
+        },
+        client => expect(client).toEqual(mockElasticClient)
+      )
+    )();
+  });
+
+  it("getElasticClient should return an error", async () => {
+    (EL.Client as jest.Mock).mockImplementationOnce(
+      () => new Error("Connection error")
+    );
+    await pipe(
+      getElasticClient(elasticNode),
+      TE.bimap(
+        err => expect(err).toEqual(new Error("Connection error")),
+        () => {
+          new Error(`it should fail while finding an existing document`);
+        }
+      )
+    )();
+  });
+});
+
+describe("createIndex", () => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("createIndex should return true", async () => {
+    mockCreate.mockResolvedValueOnce({
+      acknowledged: true
+    });
+    await pipe(
+      createIndex(mockElasticClient, indexName),
+      TE.bimap(
+        () => {
+          new Error(`it should not fail while creating index`);
+        },
+        ack => expect(ack).toEqual(true)
+      )
+    )();
+  });
+
+  it("createIndex should return an error", async () => {
+    mockCreate.mockResolvedValueOnce(new Error("Connection error"));
+    await pipe(
+      createIndex(mockElasticClient, indexName),
+      TE.bimap(
+        err => expect(err).toEqual(new Error("Connection error")),
+        () => {
+          new Error(`it should fail while finding an existing document`);
+        }
+      )
+    )();
+  });
+});
+
+describe("createIndexIfNotExists", () => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("createIndex should return true when the index does not exist", async () => {
+    mockExists.mockResolvedValueOnce(false);
+    mockCreate.mockResolvedValueOnce({
+      acknowledged: true
+    });
+    await pipe(
+      createIndexIfNotExists(mockElasticClient, indexName),
+      TE.bimap(
+        () => {
+          new Error(`it should not fail while creating index`);
+        },
+        ack => expect(ack).toEqual(true)
+      )
+    )();
+  });
+
+  it("createIndex should return true when the index exist", async () => {
+    mockExists.mockResolvedValueOnce(true);
+    await pipe(
+      createIndexIfNotExists(mockElasticClient, indexName),
+      TE.bimap(
+        () => {
+          new Error(`it should not fail while creating index`);
+        },
+        ack => expect(ack).toEqual(true)
+      )
+    )();
+    expect(mockCreate).not.toHaveBeenCalled();
+  });
+
+  it("createIndex should return an error when creating", async () => {
+    mockExists.mockResolvedValueOnce(false);
+    mockCreate.mockResolvedValueOnce(new Error("Connection error"));
+    await pipe(
+      createIndexIfNotExists(mockElasticClient, indexName),
+      TE.bimap(
+        err => expect(err).toEqual(new Error("Connection error")),
+        () => {
+          new Error(`it should fail while finding an existing document`);
+        }
+      )
+    )();
+  });
+});
+
+describe("getDocument", () => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("getDocument should return succesfully a document", async () => {
+    mockGet.mockResolvedValueOnce({
+      id: document.id
+    });
+    await pipe(
+      getDocument(mockElasticClient)(document, indexName),
+      TE.bimap(
+        () => {
+          new Error(`it should not fail while creating index`);
+        },
+        doc =>
+          expect(doc).toEqual({
+            id: document.id
+          })
+      )
+    )();
+  });
+
+  it("getDocument should return an error", async () => {
+    mockGet.mockResolvedValueOnce(new Error("Error retrieving document"));
+    await pipe(
+      getDocument(mockElasticClient)(document, indexName),
+      TE.bimap(
+        err => expect(err).toEqual(new Error("Error retrieving document")),
+        () => {
+          new Error(`it should fail while finding an existing document`);
+        }
+      )
+    )();
+  });
+});
+
+describe("indexDocument", () => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("indexDocument should index succesfully a document", async () => {
+    mockIndex.mockResolvedValueOnce({ result: "created" });
+    await pipe(
+      indexDocument(mockElasticClient)(indexName, document),
+      TE.bimap(
+        () => {
+          new Error(`it should not fail while creating index`);
+        },
+        doc => expect(doc).toEqual("created")
+      )
+    )();
+  });
+
+  it("indexDocument should return an error", async () => {
+    mockIndex.mockResolvedValueOnce(new Error("Error indexing document"));
+    await pipe(
+      indexDocument(mockElasticClient)(indexName, document),
+      TE.bimap(
+        err => expect(err).toEqual(new Error("Error indexing document")),
+        () => {
+          new Error(`it should fail while indexing a document`);
+        }
+      )
+    )();
+  });
+});
+
+describe("updateIndexDocument", () => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+  it("updateIndexDocument should index succesfully a document", async () => {
+    mockUpdate.mockResolvedValueOnce({ result: "created" });
+    await pipe(
+      updateIndexDocument(mockElasticClient)(indexName, document),
+      TE.bimap(
+        () => {
+          new Error(`it should not fail while creating index`);
+        },
+        doc => expect(doc).toEqual("created")
+      )
+    )();
+  });
+
+  it("updateIndexDocument should return an error", async () => {
+    mockUpdate.mockResolvedValueOnce(new Error("Error indexing document"));
+    await pipe(
+      updateIndexDocument(mockElasticClient)(indexName, document),
+      TE.bimap(
+        err => expect(err).toEqual(new Error("Error indexing document")),
+        () => {
+          new Error(`it should fail while indexing a document`);
+        }
+      )
+    )();
+  });
+});
---
+++ src/deduplication/elasticsearch.ts
@@ -0,0 +1,97 @@
+import * as EL from "@elastic/elasticsearch";
+import { GetResponse } from "@elastic/elasticsearch/lib/api/types";
+import { defaultLog } from "@pagopa/winston-ts";
+import * as E from "fp-ts/Either";
+import * as TE from "fp-ts/TaskEither";
+import * as B from "fp-ts/boolean";
+import { pipe } from "fp-ts/lib/function";
+
+export interface IOutputDocument {
+  readonly [key: string]: unknown;
+  readonly id: string;
+}
+
+export const getElasticClient = (
+  elasticNode: string
+): TE.TaskEither<Error, EL.Client> =>
+  TE.of(new EL.Client({ node: elasticNode }));
+
+export const createIndex = (
+  elasticClient: EL.Client,
+  indexName: string
+): TE.TaskEither<Error, boolean> =>
+  pipe(
+    TE.tryCatch(
+      () => elasticClient.indices.create({ index: indexName }),
+      E.toError
+    ),
+    TE.map(res => res.acknowledged)
+  );
+
+export const createIndexIfNotExists = (
+  elasticClient: EL.Client,
+  indexName: string
+): TE.TaskEither<Error, boolean> =>
+  pipe(
+    TE.tryCatch(
+      () => elasticClient.indices.exists({ index: indexName }),
+      E.toError
+    ),
+    TE.chain(
+      B.fold(
+        () => createIndex(elasticClient, indexName),
+        () => TE.right(true)
+      )
+    )
+  );
+
+export const getDocument = (elasticClient: EL.Client) => (
+  document: IOutputDocument,
+  indexName: string
+): TE.TaskEither<Error, GetResponse> =>
+  pipe(
+    TE.Do,
+    defaultLog.taskEither.info(`getAndIndexDocument => ${document}`),
+    () =>
+      TE.tryCatch(
+        () => elasticClient.get({ id: document.id, index: indexName }),
+        e => e as EL.errors.ResponseError
+      ),
+    defaultLog.taskEither.infoLeft(
+      e => `Error getting document from index => ${String(e)}`
+    )
+  );
+
+export const indexDocument = (elasticClient: EL.Client) => (
+  indexName: string,
+  document: IOutputDocument
+): TE.TaskEither<Error, EL.estypes.Result> =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        elasticClient.index({
+          document,
+          id: document.id,
+          index: indexName
+        }),
+      E.toError
+    ),
+    TE.map(response => response.result)
+  );
+
+export const updateIndexDocument = (elasticClient: EL.Client) => (
+  indexName: string,
+  document: IOutputDocument
+): TE.TaskEither<Error, EL.estypes.Result> =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        elasticClient.update({
+          doc: document,
+          id: document.id,
+          index: indexName
+        }),
+      E.toError
+    ),
+    TE.map(response => response.result)
+  );
