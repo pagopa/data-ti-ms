505e1ff1bb4efd72332e5c22fee3443779b9222c
AleDore <alessio85.dore@gmail.com>
2024-03-20 15:37:26 +0100

    add Native Event Hub Services

--- src/queue/eventhub/utils.ts
+++ src/queue/eventhub/utils.ts
@@ -4,9 +4,11 @@ import {
   getConsumerFromSas
 } from "@pagopa/fp-ts-kafkajs/dist/lib/KafkaConsumerCompact";
 import { AzureEventhubSasFromString } from "@pagopa/fp-ts-kafkajs/dist/lib/KafkaProducerCompact";
-
+import { EventHubConsumerClient } from "@azure/event-hubs";
+import { DefaultAzureCredential } from "@azure/identity";
 import { defaultLog } from "@pagopa/winston-ts";
 import * as E from "fp-ts/Either";
+import * as O from "fp-ts/Option";
 import { pipe } from "fp-ts/function";
 
 export const getEventHubConsumer = (
@@ -24,4 +26,46 @@ export const getEventHubConsumer = (
         ),
       getConsumerFromSas
     )
+  );
+
+export const getNativeEventHubConsumer = (
+  connectionString: string,
+  consumerGroup?: string
+): E.Either<Error, EventHubConsumerClient> =>
+  pipe(
+    AzureEventhubSasFromString.decode(connectionString),
+    E.map(() =>
+      pipe(
+        consumerGroup,
+        O.fromNullable,
+        O.getOrElse(() => "consumer-group"),
+        groupId => new EventHubConsumerClient(groupId, connectionString)
+      )
+    ),
+    E.mapLeft(() => new Error(`Error during decoding Event Hub SAS`))
+  );
+
+export const getPasswordLessNativeEventHubConsumer = (
+  hostName: string,
+  topicName: string,
+  consumerGroup?: string
+): E.Either<Error, EventHubConsumerClient> =>
+  pipe(
+    new DefaultAzureCredential(),
+    credentials =>
+      E.right(
+        pipe(
+          consumerGroup,
+          O.fromNullable,
+          O.getOrElse(() => "consumer-group"),
+          groupId =>
+            new EventHubConsumerClient(
+              groupId,
+              hostName,
+              topicName,
+              credentials
+            )
+        )
+      ),
+    E.mapLeft(() => new Error(`Error during decoding Event Hub SAS`))
   );
--- src/queue/service.ts
+++ src/queue/service.ts
@@ -6,9 +6,17 @@ import {
 } from "@pagopa/fp-ts-kafkajs/dist/lib/KafkaConsumerCompact";
 import * as E from "fp-ts/Either";
 import * as TE from "fp-ts/TaskEither";
-import { pipe } from "fp-ts/function";
+import * as AR from "fp-ts/Array";
+import { constVoid, pipe } from "fp-ts/function";
 import { EachMessageHandler } from "kafkajs";
-import { getEventHubConsumer } from "./eventhub/utils";
+import { earliestEventPosition } from "@azure/event-hubs";
+import { EventHubConsumerClient } from "@azure/event-hubs";
+import {
+  getEventHubConsumer,
+  getNativeEventHubConsumer,
+  getPasswordLessNativeEventHubConsumer
+} from "./eventhub/utils";
+
 export type MessageHandler = EachMessageHandler;
 export type QueueConsumer = KafkaConsumerCompact;
 export interface IQueueService {
@@ -35,5 +43,64 @@ export const createEventHubService = (
     getEventHubConsumer(connectionString),
     E.map(consumer => ({
       consumeMessage: defaultConsumeMessage(consumer)
+    }))
+  );
+
+const getEvhSubscriber = (
+  messageHandler: (
+    message: Record<string, unknown>
+  ) => TE.TaskEither<Error, void>
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+) => (consumer: EventHubConsumerClient) => (): TE.TaskEither<Error, void> =>
+  pipe(
+    consumer.subscribe(
+      {
+        processError: async (_err, _context) => {
+          // error reporting/handling code here
+        },
+        processEvents: async (events, _) =>
+          pipe(
+            events,
+            AR.map(msgEvt => messageHandler(msgEvt.body)),
+            AR.sequence(TE.ApplicativeSeq),
+            TE.mapLeft(err => {
+              throw err;
+            }),
+            TE.map(constVoid),
+            TE.toUnion
+          )()
+      },
+      { maxBatchSize: 1, startPosition: earliestEventPosition }
+    ),
+    TE.of,
+    TE.map(constVoid)
+  );
+
+export const createNativeEventHubService = (
+  connectionString: string,
+  messageHandler: (
+    message: Record<string, unknown>
+  ) => TE.TaskEither<Error, void>
+): E.Either<Error, IQueueService> =>
+  pipe(
+    getNativeEventHubConsumer(connectionString),
+    E.map(getEvhSubscriber(messageHandler)),
+    E.map(consumeMessage => ({
+      consumeMessage
+    }))
+  );
+
+export const createPasswordlessNativeEventHubService = (
+  hostName: string,
+  topicName: string,
+  messageHandler: (
+    message: Record<string, unknown>
+  ) => TE.TaskEither<Error, void>
+): E.Either<Error, IQueueService> =>
+  pipe(
+    getPasswordLessNativeEventHubConsumer(hostName, topicName),
+    E.map(getEvhSubscriber(messageHandler)),
+    E.map(consumeMessage => ({
+      consumeMessage
     }))
   );
